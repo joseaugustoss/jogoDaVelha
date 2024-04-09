fbeaee589d475e5e79740ecb027e366e04ec816b
José Augusto <joseaugusto0408@gmail.com>
2024-04-09 20:39:33 -0300

    feat: reset

---
+++ script.js
@@ -0,0 +1,26 @@
+// Initial data
+let square = {
+  a1: '', a2:'', a3:'',
+  b1: '', b2:'', b3:'',
+  c1: '', c2:'', c3:'',
+}
+let player = '';
+let warning = '';
+let playing =  false;
+
+
+// Events
+document.querySelector('.reset').addEventListener('click', reset);
+
+
+// Functions
+function reset(){
+  warning = '';
+  let random = Math.floor(Math.random() * 2);
+  player = random ?  '0' :  'x'
+
+  for(let i in square){
+    square[i] = '';
+  }
+  playing = true;
+}
